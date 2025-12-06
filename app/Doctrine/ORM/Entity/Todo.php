4e3bbd87ba23543a60bb1e921a8c6533261d83c9
iisyos <kosuke1216a@yahoo.co.jp>
2025-12-07 00:52:17 +0900

    XMLのパターン

--- app/Doctrine/ORM/Entity/Todo.php
+++ app/Doctrine/ORM/Entity/Todo.php
@@ -2,21 +2,10 @@
 
 namespace App\Doctrine\ORM\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity]
-#[ORM\Table(name: 'todos')]
 class Todo
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'AUTO')]
-    #[ORM\Column(type: 'integer')]
     private int $id;
-
-    #[ORM\Column(type: 'string', length: 255, nullable: false)]
     private string $title;
-
-    #[ORM\Column(type: 'boolean')]
     private bool $completed = false;
 
     public function __construct(string $title)
--- config/doctrine.php
+++ config/doctrine.php
@@ -27,10 +27,10 @@ return [
     'managers'                   => [
         'default' => [
             'dev'           => env('APP_DEBUG', false),
-            'meta'          => env('DOCTRINE_METADATA', 'attributes'),
+            'meta'          => env('DOCTRINE_METADATA', 'xml'),
             'connection'    => env('DB_CONNECTION', 'mysql'),
             'paths'         => [
-                base_path('app/Doctrine/ORM/Entity')
+                config_path('doctrine_orm_metadata')
             ],
 
             'repository'    => Doctrine\ORM\EntityRepository::class,
